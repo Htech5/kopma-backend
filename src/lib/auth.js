e0835244c168460402584c8f78c59c9571abbce4
Habib Mukhlis Triatmojo <89024106+Htech5@users.noreply.github.com>
2026-04-13 00:00:19 +0700

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,11 +3,16 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export function createToken(payload) {
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET tidak ditemukan");
+  }
+
   return jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
 }
 
 export function verifyToken(token) {
   try {
+    if (!JWT_SECRET) return null;
     return jwt.verify(token, JWT_SECRET);
   } catch {
     return null;
