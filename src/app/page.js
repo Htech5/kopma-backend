040677f010127c5ac6e1954314125c27b0e71c85
Habib Mukhlis Triatmojo <89024106+Htech5@users.noreply.github.com>
2026-05-27 15:52:23 +0700

    Update message in HomePage component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <main style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>App hidup</h1>
-      <p>Kalau halaman ini tampil, berarti Next.js berhasil diakses dari domain.</p>
+      <p>Kalau halaman ini tampil, berarti Next.js berhasil diakses dari domain, silahkan akses ke api.ukmkopmaunnes.com/login </p>
     </main>
   );
 }
